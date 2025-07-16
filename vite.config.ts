8761b1a4df3ff6f3270dcad4e4a2b41c6c039a5e
Ken Saganski <saganawski@gmail.com>
2025-07-16 16:20:14 -0400

    attempting to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/food-swipper-example/',
+  base: "https://saganawski.github.io/food-swipper-example/",
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
